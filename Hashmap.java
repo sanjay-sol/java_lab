57ef74c3c1bacb5c9e13dfc02bda806ef9a8f643
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-07-18 21:35:12 +0530

    hashmap

---
+++ Hashmap.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class HashM8a {
+ public static void main(String[] args) {
+ HashMap<Integer, String> hm = new HashMap<Integer, String>();
+ System.out.println("Is Empty or not:" + hm.isEmpty());
+ hm.put(1, "A");
+ hm.put(2, "B");
+ hm.put(3, "C");
+ System.out.println(hm);
+ hm.remove(1, "D");
+ System.out.println("Elements :" + hm);
+ hm.replace(1, "D");
+ System.out.println("Elements :" + hm);
+ System.out.println(hm.get(2));
+ hm.clear();
+ System.out.println("Elements :" + hm);
+ System.out.println("size:" + hm.size());
+ }
+}
