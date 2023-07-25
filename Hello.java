98292799e3d9be0def4d0b4cd626ac6353f0a337
Sanjay Sirangi <sanjaysirangi@gmail.com>
2023-07-25 23:13:03 +0530

    random programs

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(-1+"-1");
+        System.out.println(-1-"-1");
+        System.out.println(-1+"1");
+        System.out.println(-1"-1");
+        // System.out.println(-1+"-1");
+    }
+}
