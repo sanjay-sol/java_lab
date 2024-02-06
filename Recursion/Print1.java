b8d9f8adb2a95c39be618554892ac45504f9038e
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-02-06 23:12:28 +0530

    recusrion

--- Recursion/Print1.java
+++ Recursion/Print1.java
@@ -1,14 +1,27 @@
-package Recursion;
-
-public class Print1 {
-    public static int Maxsum(int arr[] , int k){
-       
-        return k;
-    }
+class Print1 {
     public static void main(String[] args) {
-        int[] arr = {2 , 9, 31 , -4 , 21 , 7};
-        int k = 3;
-        System.out.println(Maxsum(arr, k));
+        System.out.println(func(0, "bcaaddeaaad"));
+    }
 
+    static String func(int i, String str) {
+        // Move to the next character
+        if (str.length() == i)
+            return str;
+        if (str.charAt(i) == 'a') {
+            String str1 = removeCharAtIndex(str, i);
+            return func(i, str1);
+        }
+        i++;
+        return func(i, str);
+
+    }
+
+    static String removeCharAtIndex(String str, int index) {
+        if (index < 0 || index >= str.length()) {
+            return str; // Return the original string if index is out of bounds
+        }
+
+        // Create a new string by excluding the character at the specified index
+        return str.substring(0, index) + str.substring(index + 1);
     }
 }
