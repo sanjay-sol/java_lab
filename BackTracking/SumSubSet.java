904f6a568a5aaac6647fe0cc11ba9706b1f5840d
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-03-26 23:29:15 +0530

    backtracking subset

---
+++ BackTracking/SumSubSet.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+class SumSubSet {
+    public static void main(String[] args) {
+        int[] arr = {8,4,10,-7,17};
+        int N = arr.length;
+        int k = 22;
+         ArrayList<ArrayList<Integer>> subsets = new ArrayList<>();
+           ArrayList<Integer> currset = new ArrayList<>();
+         
+        
+     subSet(arr,N,k,0,0,currset,subsets);
+        System.out.print(subsets);
+    }
+    private static void subSet(int[] arr, int N, int K, int idx, int sum,ArrayList<Integer> currset,  ArrayList<ArrayList<Integer>> subsets){
+        if(idx == N) {
+            if(sum == K){
+                subsets.add(new ArrayList<>(currset));
+            }
+            return;
+        } 
+        
+        currset.add(arr[idx]);
+       subSet(arr,N,K,idx+1,sum+arr[idx],currset,subsets);
+       currset.remove(currset.size()-1);
+         subSet(arr,N,K,idx+1,sum,currset,subsets);
+    }
+}
